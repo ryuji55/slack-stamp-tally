649ec51debbb82a700b37989e0b1934aecf62187
ryuji55 <rt.october.2@icloud.com>
2024-02-18 11:32:04 +0900

    Slackからemojiを取得してjsonで返す

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import { WebClient } from "@slack/web-api";
+import * as dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+
+const slackClient = new WebClient(process.env.SLACK_BOT_USER_TOKEN);
+
+app.get("/emojis", async (req, res) => {
+  try {
+    const response = await slackClient.emoji.list({});
+    res.json(response.emoji);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Slackから絵文字を取得中にエラーが発生しました");
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
