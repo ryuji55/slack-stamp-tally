3b1f1f1c1852c6a429a1ff775cfa585a2de1c986
ryuji55 <rt.october.2@icloud.com>
2024-02-18 14:12:50 +0900

    Fix:thumbsupが押された投稿を返して表示

--- src/app.ts
+++ src/app.ts
@@ -8,17 +8,46 @@ const port = 3000;
 
 dotenv.config();
 
-const slackClient = new WebClient(process.env.SLACK_BOT_USER_TOKEN);
+const slackClientBot = new WebClient(process.env.SLACK_BOT_USER_TOKEN);
+const slackClientUser = new WebClient(process.env.SLACK_USER_TOKEN);
 
 app.use(cors());
 
 app.get("/emojis", async (req, res) => {
   try {
-    const response = await slackClient.emoji.list({});
+    const response = await slackClientBot.emoji.list({});
     res.json(response.emoji);
   } catch (error) {
     console.error(error);
     res.status(500).send("Slackから絵文字を取得中にエラーが発生しました");
+  }
+});
+
+app.get("/search/reactions/thumbsup", async (req, res) => {
+  try {
+    const query = "has:reaction :thumbsup:";
+    const response = await slackClientUser.search.messages({
+      query,
+      sort: "timestamp",
+      sort_dir: "desc",
+    });
+
+    console.log("response:", response);
+
+    const messages =
+      response.messages &&
+      response.messages.matches &&
+      response.messages.matches.map((match) => ({
+        user: match.username,
+        text: match.text,
+        ts: match.ts,
+        reactions: match.no_reactions,
+      }));
+
+    res.json(messages);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("リアクションの検索中にエラーが発生しました");
   }
 });
 
